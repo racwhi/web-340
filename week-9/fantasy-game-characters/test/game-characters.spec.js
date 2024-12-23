87913229d2179840d6bc24aec01d78d363bf8046
racwhi <128566218+racwhi@users.noreply.github.com>
2024-12-22 23:54:41 -0600

    week9

--- week-9/fantasy-game-characters/test/game-characters.spec.js
+++ week-9/fantasy-game-characters/test/game-characters.spec.js
@@ -1,22 +1,48 @@
+/**
+ * Author: Rachel White
+ * Date: 12/22/2024
+ * File Name: game-characters.spec.js
+ * Description: fantasy game character game-characters.spec.js
+ */
+//"use strict";
+
 // game-characters.spec.js
-const { GameCharacters } = require("../src/game-characters");
+const GameCharacters = require("../src/game-characters"); // Verify the path
 
 describe("GameCharacters", () => {
   let gameCharacters;
 
   beforeEach(() => {
-    gameCharacters = new GameCharacters();
+    gameCharacters = new GameCharacters("game-characters-data.js");
   });
 
   test("should return game characters data", (done) => {
-    // TODO: Implement this test
+    gameCharacters.getCharacters((data, error) => {
+      expect(error).toBeNull();
+      expect(data).toEqual([
+        { class: "Warrior", gender: "Male", funFact: "Loves to fight dragons" },
+        { class: "Mage", gender: "Female", funFact: "Master of illusions" },
+        { class: "Rogue", gender: "Other", funFact: "Expert in stealth" },
+      ]);
+      done();
+    });
   });
 
   test("should handle an error when the game characters data script is not found", (done) => {
-    // TODO: Implement this test
+    const invalidGameCharacters = new GameCharacters("nonexistent-script.js");
+    invalidGameCharacters.getCharacters((data, error) => {
+      expect(data).toBeNull();
+      expect(error).not.toBeNull();
+      done();
+    });
   });
 
   test("should handle an error when the game characters data script fails", (done) => {
-    // TODO: Implement this test
+    const failingGameCharacters = new GameCharacters("failing-script.js");
+    failingGameCharacters.getCharacters((data, error) => {
+      expect(data).toBeNull();
+      expect(error).not.toBeNull();
+      done();
+    });
   });
 });
